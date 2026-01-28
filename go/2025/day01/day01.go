414c3642842bc0851c11652bfc7fbc5975630387
Panji Dinata <panjidinata88@gmail.com>
2026-01-28 16:45:43 +0800

    add function to find number of time sequence end at zero

--- go/2025/day01/day01.go
+++ go/2025/day01/day01.go
@@ -1,6 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func GetNumOfDialAtZero(rotationSeq []string) (int, error) {
+	if len(rotationSeq) == 0 {
+		return 0, errors.New("rotation sequence is empty")
+	}
+
+	currHead := 50
+	result := 0
+
+	for _, seq := range rotationSeq {
+		if strings.HasPrefix(seq, "L") {
+			after, _ := strings.CutPrefix(seq, "L")
+			if num, err := strconv.Atoi(after); err == nil {
+				currHead = currHead - num
+				if (currHead % 100) == 0 {
+					currHead = 0
+					result += 1
+				} else if currHead < 0 {
+					currHead += 100
+					if currHead > 100 {
+						currHead = currHead % 100
+					}
+				}
+			}
+		} else if strings.HasPrefix(seq, "R") {
+			after, _ := strings.CutPrefix(seq, "R")
+			if num, err := strconv.Atoi(after); err == nil {
+				currHead = currHead + num
+				if (currHead % 100) == 0 {
+					currHead = 0
+					result += 1
+				} else if currHead > 100 {
+					currHead = currHead % 100
+				}
+			}
+		}
+	}
+
+	return result, nil
+}
 
 func main() {
 	fmt.Println("Hello, World")
