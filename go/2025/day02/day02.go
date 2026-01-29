0a0a4dbb7d7a4af4bcb415d76f0c883cfb7a0f89
Panji Dinata <panjidinata88@gmail.com>
2026-01-29 10:36:44 +0800

    day 2 challge

---
+++ go/2025/day02/day02.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+	   1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+	   824824821-824824827,2121212118-2121212124
+
+	   get invalid id from list of number above. id is invalid when a number repeated twice.
+	   22 is invalid because 2 is repeated
+	   1111 is invalid because 11 is repeated twice. etc.,
+
+	   0101 is valid becase it start with zero */
+
+	fmt.Println("this is day 2 of advent of code 2025")
+}
